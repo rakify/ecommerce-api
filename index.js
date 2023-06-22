69246c1f77fbdf8a70074ce48160300fbed3d722
rakify <irakibm@gmail.com>
2023-06-22 22:27:08 +0600

    revert change

--- index.js
+++ index.js
@@ -32,7 +32,12 @@ mongoose
 //we should use them before routes
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5000",
+      "http://localhost:5001",
+      "https://bestmart.vercel.app",
+      "https://bestmart-admin.vercel.app",
+    ],
     credentials: true, //access-control-allow-credentials:true
   })
 );
